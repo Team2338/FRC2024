3ec48b92c98325c3578c8e99e66c986b1cb42dc9
bobsfriend12 <calebsethherring+github@gmail.com>
2024-01-12 09:35:30 -0600

    remove unnecessary import

--- src/main/java/team/gif/robot/Robot.java
+++ src/main/java/team/gif/robot/Robot.java
@@ -6,7 +6,6 @@ package team.gif.robot;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import team.gif.robot.subsystems.drivers.Limelight;
