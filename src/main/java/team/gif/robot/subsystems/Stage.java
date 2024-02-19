64a6051771bfe5d84fd8f5e07757df3efec8b3c6
Bill Knopf <billknopfjr@yahoo.com>
2024-02-19 12:20:53 -0600

    stage on shuffleboard and shooter angle condition

--- src/main/java/team/gif/robot/subsystems/Stage.java
+++ src/main/java/team/gif/robot/subsystems/Stage.java
@@ -1,6 +1,7 @@
 package team.gif.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.Constants;
 import team.gif.robot.Robot;
 
 public class Stage extends SubsystemBase {
@@ -8,6 +9,6 @@ public class Stage extends SubsystemBase {
     public Stage(){}
 
     public boolean getSafe() {
-        return (Robot.climber.getPosition() < 100 && Robot.elevator.getPosition() < 100 && Robot.shooter.getPosition() < 100);
+        return Robot.shooter.getPosition() <= (50.0 - Constants.ShooterRotation.MIN_LIMIT_DEGREES) * Constants.ShooterRotation.ABSOLUTE_PER_DEGREE + Constants.ShooterRotation.MIN_LIMIT_ABSOLUTE;//(Robot.climber.getPosition() < 100 && Robot.elevator.getPosition() < 100 && Robot.shooter.getPosition() < 100);
     }
 }
