fd695f63e0229f388b6dee9a6337d379d8c6c145
Bill Knopf <billknopfjr@yahoo.com>
2024-02-22 22:46:36 -0600

    Move value to constant

--- src/main/java/team/gif/robot/subsystems/Stage.java
+++ src/main/java/team/gif/robot/subsystems/Stage.java
@@ -9,6 +9,6 @@ public class Stage extends SubsystemBase {
     public Stage(){}
 
     public boolean getSafe() {
-        return Robot.shooter.getPosition() <= Robot.shooter.degreesToAbsolute(50);//(Robot.climber.getPosition() < 100 && Robot.elevator.getPosition() < 100 && Robot.shooter.getPosition() < 100);
+        return Robot.shooter.getPosition() <= Robot.shooter.degreesToAbsolute(Constants.ShooterRotation.STAGE_SAFE_DEGREES);//(Robot.climber.getPosition() < 100 && Robot.elevator.getPosition() < 100 && Robot.shooter.getPosition() < 100);
     }
 }
