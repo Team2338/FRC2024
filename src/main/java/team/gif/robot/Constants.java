03209c8c0f639b46c1ac2e51d72ddcd3d0c0f561
Bill Knopf <billknopfjr@yahoo.com>
2024-05-02 20:12:17 -0500

    WIP

--- src/main/java/team/gif/robot/Constants.java
+++ src/main/java/team/gif/robot/Constants.java
@@ -397,7 +397,7 @@ public final class Constants {
 
         // Encoder setpoints and values
         // These are the encoder specific values
-        public static final double ENCODER_OFFSET_ABSOLUTE = -0.627783203125;// this is determined either manually or via the auto-calibration
+        public static final double ENCODER_OFFSET_ABSOLUTE = -0.74130859375;// this is determined either manually or via the auto-calibration
         public static final double ABSOLUTE_PER_DEGREE = 0.00647249; // 0.008333;
         // These are the values we want the bot to utilize
         public static final double KILL_LIMIT_ABSOLUTE = 0.87;
